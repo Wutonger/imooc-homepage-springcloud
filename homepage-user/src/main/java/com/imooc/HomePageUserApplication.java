8978fa5046bbee0dfd601975831787ed90466611
loux <1176160102@qq.com>
2020-02-15 20:23:29 +0800

    Hystrix简化配置

--- homepage-user/src/main/java/com/imooc/HomePageUserApplication.java
+++ homepage-user/src/main/java/com/imooc/HomePageUserApplication.java
@@ -2,13 +2,15 @@ package com.imooc;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringCloudApplication
+@SpringBootApplication
 @EnableFeignClients  //使用Feign远程调用
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class HomePageUserApplication {
 
     public static void main(String[] args) {
